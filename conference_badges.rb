b104e822426cdcdf729fd2608547a0011c4a4817
Richard Lee <richlee81192@gmail.com>
2019-09-18 16:28:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  ray = []
+  arr.each { |arr| ray << "Hello, my name is #{arr}."}
+  ray
+end
+
+def assign_room(speaker)
+  
+end 
